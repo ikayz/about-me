fddcb23568fdccf00646338df2264207866f4ed6
Isaac Miti <ikayz@live.com>
2022-01-07 14:24:06 +0200

    Implement data binding

--- app/src/main/java/com/ikayz/kotlin/aboutme/MainActivity.kt
+++ app/src/main/java/com/ikayz/kotlin/aboutme/MainActivity.kt
@@ -8,24 +8,29 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.ikayz.kotlin.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        findViewById<Button>(R.id.done_button).setOnClickListener {
+        binding.doneButton.setOnClickListener {
             addNickname(it)
         }
     }
 
     private fun addNickname(view: View) {
-        val nickname = findViewById<EditText>(R.id.nickname_edit)
-        val displayNickname = findViewById<TextView>(R.id.nickname_text)
-        displayNickname.text = nickname.text
-        nickname.visibility = View.GONE
-        displayNickname.visibility = View.VISIBLE
-        view.visibility = View.GONE
+        binding.apply {
+            nicknameText.text = binding.nicknameEdit.text
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
 
         // Hide the keyboard after user action
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
